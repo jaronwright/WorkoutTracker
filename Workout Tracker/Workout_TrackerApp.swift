65b27980a94fbc1cf2d1badf44bdf0201276c750
Jaron Wright <jaron.code@gmail.com>
2025-07-03 18:34:38 -0500

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Jaron Wright on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Workout TrackerTests/Workout_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerTests.swift
+//  Workout TrackerTests
+//
+//  Created by Jaron Wright on 7/3/25.
+//
+
+import Testing
+@testable import Workout_Tracker
+
+struct Workout_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
